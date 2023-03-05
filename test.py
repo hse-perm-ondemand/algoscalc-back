966f718ebe818b2544c826c99e9da3c631baffc4
MikhailovAlexander <45492097+MikhailovAlexander@users.noreply.github.com>
2023-03-05 14:19:17 +0500

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import unittest
 
 class TestHello(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(1, 2)
+        x = 'unusable variable'
+        self.assertEqual(1, 1)
 
 
 if __name__ == '__main__':
