8d8a40e94729aa422f7018559dcc78b3254d9354
white team <novoselovaalice2002@mail.ru>
2023-05-04 13:58:38 +0500

    Реализован алгоритм поиска корней квадратного уравнения

---
+++ src/algorithms/quadratic_equation/function.py
@@ -0,0 +1,27 @@
+from math import sqrt
+def quadratic_equation(a: float, b: float, c: float) -> str:
+    if a == 0:
+        return 'Коэффицент при х^2 в квадратном уравнении не может быть равен 0!'
+    else:
+        discriminant = b ** 2 - 4 * a * c
+        if discriminant < 0:
+            return 'Действительных корней нет, т. к. D < 0'
+        elif discriminant == 0:
+            x = -b / (2 * a)
+            return 'Корень только один: x = ' + str(x)
+        else:
+            x1 = (-b + sqrt(discriminant)) / (2 * a)
+            x2 = (-b - sqrt(discriminant)) / (2 * a)
+            return 'x1 = ' + (str(round(x1, 8))) + ',' + ' x2 = ' + str((round(x2, 8)))
+
+
+def main(a: float, b: float, c: float):
+    return {'roots': quadratic_equation(a, b, c)}
+
+
+if __name__ == '__main__':
+    a = 1
+    b = 1
+    c = 1
+    print(quadratic_equation(a, b, c))
+
