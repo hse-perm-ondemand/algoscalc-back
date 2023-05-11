392718affea8c459ada3169c58433cf3e8da7376
Dima <112894460+DimaChepuchtanov@users.noreply.github.com>
2023-05-11 15:33:21 +0500

    Update tests.py

--- src/algorithms/substring_in_a_string/tests.py
+++ src/algorithms/substring_in_a_string/tests.py
@@ -1,40 +1,40 @@
 import unittest
 
 
-from src.algorithms.substring_in_a_string.function import findcountstring
+from src.algorithms.substring_in_a_string.function import main
 
 
 class TestCase(unittest.TestCase):
     def test_type_of_coefficient(self):
         self.assertRaisesRegex(ValueError,
                                "Значения не строковые",
-                               findcountstring,"one",0)
+                               main, "one", 0)
 
     def test_zero_a_coefficient(self):
         self.assertRaisesRegex(ValueError,
                                "Значения не строковые",
-                               findcountstring,0,0)
+                               main, 0, 0)
 
     def test_zero_b_coefficient(self):
         self.assertRaisesRegex(ValueError,
                                "Значения не строковые",
-                               findcountstring,0,"ищу")
+                               main, 0, "ищу")
 
     def test_zero_c_coefficient(self):
-        self.assertEqual(findcountstring("Hello world world hello hello hello world","hello world"),
-                             {"num_count": 3})
+        self.assertEqual(main("Hello world world hello hello hello world","hello world"),
+                             {'num_count': 2})
 
     def test_negative_discriminant(self):
-        self.assertEqual(findcountstring("it is very long text","i"),
-                              {"num_count": 2})
+        self.assertEqual(main("it is very long text","i"),
+                             {'num_count': 2})
 
     def test_zero_discriminant(self):
-        self.assertEqual(findcountstring("text is very long","text is very longe"),
-                          {"num_count": 0})
+        self.assertEqual(main("text is very long","text is very longe"),
+                          {'num_count': 0})
 
     def test_periodic_fraction_coefficient(self):
-        self.assertEqual(findcountstring("text is very long","text is very long"),
-                          {"num_count": 1})
+        self.assertEqual(main("text is very long","text is very long"),
+                          {'num_count': 1})
 
 if __name__ == '__main__':
     unittest.main()
