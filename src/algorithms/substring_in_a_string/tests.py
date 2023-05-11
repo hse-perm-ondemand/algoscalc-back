a492ed463c2bd264c073167ed92b8196be598001
Dima <112894460+DimaChepuchtanov@users.noreply.github.com>
2023-05-11 15:21:23 +0500

    Update tests.py

--- src/algorithms/substring_in_a_string/tests.py
+++ src/algorithms/substring_in_a_string/tests.py
@@ -22,19 +22,19 @@ class TestCase(unittest.TestCase):
 
     def test_zero_c_coefficient(self):
         self.assertEqual(findcountstring("Hello world world hello hello hello world","hello world"),
-                             'Num_count = 2')
+                             {"num_count": 3})
 
     def test_negative_discriminant(self):
         self.assertEqual(findcountstring("it is very long text","i"),
-                             'Num_count = 2')
+                              {"num_count": 2})
 
     def test_zero_discriminant(self):
         self.assertEqual(findcountstring("text is very long","text is very longe"),
-                         'Num_count = 0')
+                          {"num_count": 0})
 
     def test_periodic_fraction_coefficient(self):
         self.assertEqual(findcountstring("text is very long","text is very long"),
-                         'Num_count = 1')
+                          {"num_count": 1})
 
 if __name__ == '__main__':
     unittest.main()
