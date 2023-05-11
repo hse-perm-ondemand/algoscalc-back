3ad39904d8c8dc5325d79ac4526b8e970a49b1a8
Dima <112894460+DimaChepuchtanov@users.noreply.github.com>
2023-05-11 15:44:31 +0500

    Update tests.py

--- src/algorithms/substring_in_a_string/tests.py
+++ src/algorithms/substring_in_a_string/tests.py
@@ -5,35 +5,25 @@ from src.algorithms.substring_in_a_string.function import main
 
 
 class TestCase(unittest.TestCase):
-    def test_type_of_coefficient(self):
+    def test_chek_value(self):
         self.assertRaisesRegex(ValueError,
                                "Значения не строковые",
                                main, "one", 0)
 
-    def test_zero_a_coefficient(self):
-        self.assertRaisesRegex(ValueError,
-                               "Значения не строковые",
-                               main, 0, 0)
-
-    def test_zero_b_coefficient(self):
-        self.assertRaisesRegex(ValueError,
-                               "Значения не строковые",
-                               main, 0, "ищу")
-
-    def test_zero_c_coefficient(self):
+    def test_long_text(self):
         self.assertEqual(main("Hello world world hello hello hello world","hello world"),
                              {'num_count': 2})
 
-    def test_negative_discriminant(self):
+    def test_short_findtext(self):
         self.assertEqual(main("it is very long text","i"),
                              {'num_count': 2})
 
-    def test_zero_discriminant(self):
-        self.assertEqual(main("text is very long","text is very longe"),
+    def test_short_them_text(self):
+        self.assertEqual(main("text is ","text is very longe"),
                           {'num_count': 0})
 
-    def test_periodic_fraction_coefficient(self):
-        self.assertEqual(main("text is very long","text is very long"),
+    def test_equel_texts(self):
+        self.assertEqual(main("texts","texts"),
                           {'num_count': 1})
 
 if __name__ == '__main__':
